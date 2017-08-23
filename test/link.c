4549469655e6ac35bc657de98dcd77254ffa2684
Mike Gray <mgray@aph.org>
2017-08-23 15:29:25 -0400

    Refactored test-link output

--- test/link.c
+++ test/link.c
@@ -6,21 +6,23 @@
 
 /******************************************************************************/
 
+static FILE *output;
+
 static void log_callback(const int level, const char *message)
 {
 	switch(level)
 	{
-	case LOUIS_LOG_ALL:      fprintf(stderr, "ALL:  ");      break;
-	case LOUIS_LOG_TRACE:    fprintf(stderr, "TRACE:  ");    break;
-	case LOUIS_LOG_DEBUG:    fprintf(stderr, "DEBUG:  ");    break;
-	case LOUIS_LOG_INFO:     fprintf(stderr, "INFO:  ");     break;
-	case LOUIS_LOG_WARNING:  fprintf(stderr, "WARNING:  ");  break;
-	case LOUIS_LOG_ERROR:    fprintf(stderr, "ERROR:  ");    break;
-	case LOUIS_LOG_FATAL:    fprintf(stderr, "FATAL:  ");    break;
+	case LOUIS_LOG_ALL:      fprintf(output, "ALL:  ");      break;
+	case LOUIS_LOG_TRACE:    fprintf(output, "TRACE:  ");    break;
+	case LOUIS_LOG_DEBUG:    fprintf(output, "DEBUG:  ");    break;
+	case LOUIS_LOG_INFO:     fprintf(output, "INFO:  ");     break;
+	case LOUIS_LOG_WARNING:  fprintf(output, "WARNING:  ");  break;
+	case LOUIS_LOG_ERROR:    fprintf(output, "ERROR:  ");    break;
+	case LOUIS_LOG_FATAL:    fprintf(output, "FATAL:  ");    break;
 	}
 
-	fprintf(stderr, "%s\n", message);
-	fflush(stderr);
+	fprintf(output, "%s\n", message);
+	fflush(output);
 }
 
 /*   copied from utf.h   */
@@ -91,159 +93,274 @@ int utf16le_convert_to_utf8(char *cchars, const int cchars_max, const unsigned s
 
 int main(void)
 {
-	void (*callback)(const int level, const char *message);
 	unsigned short trans[] = u"⠕⠪⠀⠕⠪⠕⠪⠀⠕⠕⠪⠪⠀⠕⠪⠕⠪⠀⠕⠪";
 	unsigned short snart[] = u"x xx xxx xx x";
 	unsigned short convert[] = u">< ><>< >><< ><>< ><";
 	unsigned short trevnoc[] = u">< ><>< >><< ><>< ><";
 
-	char cchars[0x100] = {0};
-	unsigned short uchars[0x100] = {0};
+	void (*callback)(const int level, const char *message);
+	char cchars[0x100];
+	unsigned short uchars[0x100];
 	int uchars_len, i;
 
-	puts("test link");
+	memset(cchars, 0, 0x100);
+	memset(uchars, 0, 0x100 * sizeof(unsigned short));
 
-	puts("louis_set_log_callback");
-	louis_set_log_callback(log_callback);
+	output = fopen("build/test/output-link.txt", "w");
+	if(!output)
+		output = stdout;
 
-	printf("louis_get_log_callback");
-	callback = louis_get_log_callback();
-	printf("    = 0x%lx\n", (unsigned long)callback);
-	if(callback != log_callback)
-	{
-		fprintf(stderr, "ERROR:  louis_get_log_callback\n");
-		return 1;
-	}
+	if(output != stdout)
+		puts("testing link");
+
+	fputs("testing link\n", output);
+
 
 	louis_get_version(cchars, 0x100);
-	printf("version                   = %s\n", cchars);
+	fprintf(output, "version                   = %s\n", cchars);
 
-	puts("louis_set_paths          := test");
+
+	fprintf(output, "louis_get_log_callback");
+	if(output != stdout)
+		printf("louis_get_log_callback:  ");
+	callback = louis_get_log_callback();
+	fprintf(output, "    = 0x%lx\n", (unsigned long)callback);
+	if(callback)
+	{
+		fprintf(output, "ERROR:  louis_get_log_callback\n");
+		if(output != stdout)
+			puts("FAIL");
+		return 1;
+	}
+	if(output != stdout)
+		puts("PASS");
+
+
+	fprintf(output, "louis_set_log_callback:  := 0x%lx\n", (unsigned long)log_callback);
+	if(output != stdout)
+		printf("louis_set_log_callback:  ");
+	louis_set_log_callback(log_callback);
+
+	fprintf(output, "louis_get_log_callback");
+	callback = louis_get_log_callback();
+	fprintf(output, "    = 0x%lx\n", (unsigned long)callback);
+	if(callback != log_callback)
+	{
+		fprintf(output, "ERROR:  louis_get_log_callback\n");
+		if(output != stdout)
+			puts("FAIL");
+		return 1;
+	}
+	if(output != stdout)
+		puts("PASS");
+
+
+	fprintf(output, "louis_get_paths");
+	if(output != stdout)
+		printf("louis_get_paths:  ");
+	if(louis_get_paths(cchars, 0x100))
+	{
+		fprintf(output, "\nERROR:  louis_get_paths\n");
+		if(output != stdout)
+			puts("FAIL");
+		return 1;
+	}
+	fprintf(output, "           = %s\n", cchars);
+	if(cchars[0])
+	{
+		fprintf(output, "\nERROR:  louis_get_paths not \"test\"\n");
+		if(output != stdout)
+			puts("FAIL");
+		return 1;
+	}
+	if(output != stdout)
+		puts("PASS");
+
+
+	fputs("louis_set_paths          := test\n", output);
+	if(output != stdout)
+		printf("louis_set_paths:  ");
 	if(!louis_set_paths("test"))
 	{
-		fprintf(stderr, "ERROR:  louis_set_paths\n");
+		fprintf(output, "ERROR:  louis_set_paths\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 
-	printf("louis_get_paths");
-	fflush(stdout);
+	fprintf(output, "louis_get_paths");
 	if(!louis_get_paths(cchars, 0x100))
 	{
-		fprintf(stderr, "\nERROR:  louis_get_paths\n");
+		fprintf(output, "\nERROR:  louis_get_paths\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
-	printf("           = %s\n", cchars);
+	fprintf(output, "           = %s\n", cchars);
 	if(strncmp(cchars, "test", 5))
 	{
-		fprintf(stderr, "\nERROR:  louis_get_paths not \"test\"\n");
+		fprintf(output, "\nERROR:  louis_get_paths not \"test\"\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
+	if(output != stdout)
+		puts("PASS");
 
-	puts("louis_add_paths          := test/tables");
+
+	fputs("louis_add_paths          := test/tables\n", output);
+	if(output != stdout)
+		printf("louis_add_paths:  ");
 	if(!louis_add_paths("test/tables"))
 	{
-		fprintf(stderr, "ERROR:  louis_add_paths\n");
+		fprintf(output, "ERROR:  louis_add_paths\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 
-	printf("louis_get_paths");
-	fflush(stdout);
+	fprintf(output, "louis_get_paths");
 	if(!louis_get_paths(cchars, 0x100))
 	{
-		fprintf(stderr, "\nERROR:  louis_get_paths\n");
+		fprintf(output, "\nERROR:  louis_get_paths\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
-	printf("           = %s\n", cchars);
+	fprintf(output, "           = %s\n", cchars);
 	if(strncmp(cchars, "test:test/tables", 17))
 	{
-		fprintf(stderr, "\nERROR:  louis_get_paths not \"test:test/tables\"\n");
+		fprintf(output, "ERROR:  louis_get_paths not \"test:test/tables\"\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
+	if(output != stdout)
+		puts("PASS");
 
 
-	printf("louis_translate_forward");
-	fflush(stdout);
+	fprintf(output, "louis_translate_forward");
+	if(output != stdout)
+		printf("louis_translate_forward:  ");
 	uchars_len = louis_translate_forward(uchars, 0x100, u"x xx xxx xx x", 13, "link.rst", NULL, NULL, NULL, NULL);
 	if(uchars_len < 1)
 	{
-		fprintf(stderr, "\nERROR:  louis_translate_forward FAIL\n");
+		fprintf(output, "\nERROR:  louis_translate_forward FAIL\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 	utf16le_convert_to_utf8(cchars, 0x100, uchars, uchars_len);
-	printf("   = %s\n", cchars);
+	fprintf(output, "   = %s\n", cchars);
 
 	if(uchars_len != 20)
 	{
-		fprintf(stderr, "ERROR:  louis_translate_forward FAIL:  length\n");
+		fprintf(output, "ERROR:  louis_translate_forward FAIL:  length\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 	for(i = 0; i < uchars_len; i++)
 	if(trans[i] != uchars[i])
 	{
-		fprintf(stderr, "ERROR:  louis_translate_forward FAIL:  trans\n");
+		fprintf(output, "ERROR:  louis_translate_forward FAIL:  trans\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
+	if(output != stdout)
+		puts("PASS");
 
 
-	printf("louis_translate_backward");
-	fflush(stdout);
+	fprintf(output, "louis_translate_backward");
+	if(output != stdout)
+		printf("louis_translate_backward:  ");
 	uchars_len = louis_translate_backward(uchars, 0x100, u"⠕⠪ ⠕⠪⠕⠪ ⠕⠕⠪⠪ ⠕⠪⠕⠪ ⠕⠪", 20, "link.rst", NULL, NULL, NULL, NULL);
 	if(uchars_len < 1)
 	{
-		fprintf(stderr, "\nERROR:  louis_translate_backward\n");
+		fprintf(output, "\nERROR:  louis_translate_backward\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 	utf16le_convert_to_utf8(cchars, 0x100, uchars, uchars_len);
-	printf("  = %s\n", cchars);
+	fprintf(output, "  = %s\n", cchars);
 
 	if(uchars_len != 13)
 	{
-		fprintf(stderr, "ERROR:  louis_translate_backward FAIL\n");
+		fprintf(output, "ERROR:  louis_translate_backward FAIL\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 	for(i = 0; i < uchars_len; i++)
 	if(snart[i] != (char)uchars[i])
 	{
-		fprintf(stderr, "ERROR:  louis_translate_backward FAIL\n");
+		fprintf(output, "ERROR:  louis_translate_backward FAIL\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
+	if(output != stdout)
+		puts("PASS");
 
 
-	printf("louis_convert_to");
-	fflush(stdout);
+	fprintf(output, "louis_convert_to");
+	if(output != stdout)
+		printf("louis_convert_to:  ");
 	utf16_copy(uchars, trans, 21);
 	if(!louis_convert_to(uchars, 20, "link.cvt"))
 	{
-		fprintf(stderr, "\nERROR:  louis_convert_to\n");
+		fprintf(output, "\nERROR:  louis_convert_to\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 	utf16le_convert_to_utf8(cchars, 0x100, uchars, 20);
-	printf("          = %s\n", cchars);
+	fprintf(output, "          = %s\n", cchars);
 
 	for(i = 0; i < 20; i++)
 	if(convert[i] != uchars[i])
 	{
-		fprintf(stderr, "ERROR:  louis_convert_to FAIL conversion\n");
+		fprintf(output, "ERROR:  louis_convert_to FAIL conversion\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
+	if(output != stdout)
+		puts("PASS");
 
 
-	printf("louis_convert_from");
-	fflush(stdout);
+	fprintf(output, "louis_convert_from");
+	if(output != stdout)
+		printf("louis_convert_from:  ");
 	utf16_copy(uchars, trevnoc, 21);
 	if(!louis_convert_from(uchars, 20, "link.cvt"))
 	{
-		fprintf(stderr, "\nERROR:  louis_convert_from\n");
+		fprintf(output, "\nERROR:  louis_convert_from\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
 	}
 	utf16le_convert_to_utf8(cchars, 0x100, uchars, 20);
-	printf("        = %s\n", cchars);
+	fprintf(output, "        = %s\n", cchars);
 
 	for(i = 0; i < 20; i++)
 	if(trans[i] != uchars[i])
 	{
-		fprintf(stderr, "ERROR:  louis_convert_from FAIL conversion\n");
+		fprintf(output, "ERROR:  louis_convert_from FAIL conversion\n");
+		if(output != stdout)
+			puts("FAIL");
 		return 1;
+	}
+	if(output != stdout)
+		puts("PASS");
+
+
+	if(output != stdout)
+	{
+		puts("");
+		fclose(output);
 	}
 
 	return 0;
